4d3947ee77ee2d82efa47a23e5dcef7391013947
KulpaPiotr <piokul83@gmail.com>
2022-09-04 10:47:44 +0200

    Initial commit

---
+++ src/main/java/com/presidents/service/president/PresidentService.java
@@ -0,0 +1,23 @@
+package com.presidents.service.president;
+
+import com.presidents.model.dto.PresidentDto;
+
+import java.util.List;
+import java.util.Set;
+
+public interface PresidentService {
+
+    List<PresidentDto> getAllPresidents();
+
+    Set<PresidentDto> findPresidentsByName(String name);
+
+    Set<PresidentDto> findPresidentsByPoliticalParty(String party);
+
+    PresidentDto savePresident(PresidentDto presidentDto);
+
+    PresidentDto updatePresident(PresidentDto presidentDto);
+
+    PresidentDto updatePresidentPartial(PresidentDto presidentDto);
+
+    void deletePresident(Long id);
+}
